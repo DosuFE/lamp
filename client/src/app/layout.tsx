4624927f01b77a12627ded271891c933ef7df30f
DosuFE <sdosu@dervac.com>
2026-04-22 12:57:48 +0100

    Implementing of Favicon Into the Site

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "LAMP",
   description: "Learning & Assessment Management Portal",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
